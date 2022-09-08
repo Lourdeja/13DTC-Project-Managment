c91f30ffe24bc3a6e292d5271b338dc95a575ee7
Lourdeja <jaron.lourdes@student.onslow.school.nz>
2022-09-09 10:27:24 +1200

    Questions have been created Will remove later to begin code. They are on serperate file.

---
+++ assesment_V1.py
@@ -0,0 +1,61 @@
+##Questions
+#Q1 What is Inheritance
+    #A1 classes inheriting from other classes (true)
+    #A2 dna you inherit from parents
+    #A3 money you inherit from dead grandparents
+    #A4 the inherit will to outcast the vast universe
+
+#Q2 What is a base class?  
+    #an object-oriented programming language, from which other classes are derived (true)
+    #a mere peasant to society
+    # An object-oriented programming language, that stores objects
+    #the base fundemtentals of object oriented
+
+#Q3 What is a super class
+    #the class from which many subclasses can be created (true)
+    #the controlling class of the objects
+    #the ultimate lifeform
+    #supernated class object code
+
+#Q4 What is the main benefit of OOP?
+    #Makes it much easier to create 3D objects, such as cubes, spheres for visual design 
+    #Modularity: Encapsulation enables objects to be self-contained, making troubleshooting and collaborative development easier. (true)
+    #Ooopsie doopsie
+    #to concur code is validated and evaluated for object oriented playgrounds.
+
+#Q5 Which of these is not related to OOP?
+    #class
+    #object
+    #method
+    #mugiwara (true)
+
+#Q6 An __ is an instance of class
+    #object (true)
+    #inheritance
+    #variable
+    #cheese
+
+#Q7 A __ is a template or blueprint from which objects can be instantiated from.
+    #class (true)
+    #integer
+    #turnip
+    #diaphram
+
+#Q8 OOP stands for Object ______ Programming?
+    #oriented (true)
+    #originated
+    #opium
+    #odiss
+
+#Q9 When sub class declares a method that has the same type of arguments as a method declared by 
+#one of its superclasses, it is termed as?
+    #Method overriding (true)
+    #Microwave transaction
+    #Operator overloading
+    #Method overloading
+
+#Q10 Can objects of abstract classes be instantiated?
+    #No (true)
+    #Yes
+    #depends on circumstance
+    #idk
